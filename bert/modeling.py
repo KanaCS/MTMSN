af396588e7136b3ca7c06d90d844f242cf92efc3
KanaCS <49162615+KanaCS@users.noreply.github.com>
2021-10-26 22:21:50 +0800

    Update modeling.py

--- bert/modeling.py
+++ bert/modeling.py
@@ -142,7 +142,7 @@ class BERTEmbeddings(nn.Module):
         self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size)
         self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
         self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
-        self.type_embeddings = nn.Embedding(8, config.hidden_size)
+        self.type_embeddings = nn.Embedding(19+1, config.hidden_size)
         # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
         # any TensorFlow checkpoint file
         self.LayerNorm = BERTLayerNorm(config)
